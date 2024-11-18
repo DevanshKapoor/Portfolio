e47bea90898ae960ae28082871100f5055276ae1
DevanshKapoor <kapoordevansh368@gmail.com>
2024-11-19 02:25:13 +0530

    Update Me.jsx

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,12 +1,12 @@
 import AboutContent from "./MeContent"
-
+import me from "../assets/MyImage.png"
 const Me = () => {
   return (
     <>
     <div className="flex justify-between p-10">
         <AboutContent/>
         <image className="roboto-bold">
-            <img src="src/assets/MyImage.png" alt="my image" className=" lg:h-[80vh] drop-shadow-[0_0_3px_white]"/>
+            <img src={me} alt="my image" className=" lg:h-[80vh] drop-shadow-[0_0_3px_white]"/>
         </image>
         
     </div>
